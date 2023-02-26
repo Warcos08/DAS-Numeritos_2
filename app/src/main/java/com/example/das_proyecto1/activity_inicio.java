d07b2bbd6104e5fde241b1bc5e608ac98d7fc74d
Warcos08 <mmerino08p@gmail.com>
2023-02-26 12:45:20 +0100

    dialog salir y xml idiomas

--- app/src/main/java/com/example/das_proyecto1/activity_inicio.java
+++ app/src/main/java/com/example/das_proyecto1/activity_inicio.java
@@ -1,8 +1,12 @@
 package com.example.das_proyecto1;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_inicio extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class activity_inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+    }
+
+    public void onClickSalir(View v) {
+        DialogFragment dialogo_salir = new dialogo_salir();
+        dialogo_salir.show(getSupportFragmentManager(), "dialogo_salir");
+    }
+
+    // Para guardar la info cuando se rote la pantalla
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
     }
 }
---
+++ app/src/main/java/com/example/das_proyecto1/dialogo_salir.java
@@ -0,0 +1,50 @@
+package com.example.das_proyecto1;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.LocaleList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Locale;
+
+public class dialogo_salir extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        super.onCreateDialog(savedInstanceState);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        // Obtengo el idioma del dispositivo (NOSE SI EL DE LA APP)
+        Locale locale = getResources().getConfiguration().getLocales().get(0);
+
+        String idioma = locale.getDisplayLanguage();
+        String msg;
+        String si;
+        if (idioma.equals("English")) {
+            msg = "Are you sure you want to quit?";
+            si = "Yes";
+        } else {
+            msg = "¿Estás seguro que quieres salir?";
+            si = "Sí";
+        }
+
+        builder.setMessage(msg);
+        builder.setNegativeButton("No", null); // La accion de cerrar el dialogo se realiza por defecto
+        builder.setPositiveButton(si, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                System.exit(0);
+            }
+        });
+
+        // Devuelvo el dialogo creado por el builder
+        return builder.create();
+    }
+}
