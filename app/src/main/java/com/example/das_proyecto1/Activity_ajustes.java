5b615590264bbb1e7c473ad1102084cbe2ad0e89
Warcos08 <mmerino08p@gmail.com>
2023-03-11 13:57:54 +0100

    notificacion salir de ajustes (sin imagen)

--- app/src/main/java/com/example/das_proyecto1/Activity_ajustes.java
+++ app/src/main/java/com/example/das_proyecto1/Activity_ajustes.java
@@ -2,10 +2,19 @@ package com.example.das_proyecto1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -94,10 +103,35 @@ public class Activity_ajustes extends AppCompatActivity {
         });
 
         // Volver a inicio
-        Button btn_guardar = (Button) findViewById(R.id.ajustes_btn_salir);
-        btn_guardar.setOnClickListener(new View.OnClickListener() {
+        Button btn_salir = (Button) findViewById(R.id.ajustes_btn_salir);
+        btn_salir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Mando una notificacion local de que se han guardado los cambios
+                if (ContextCompat.checkSelfPermission(Activity_ajustes.this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    // Pido los permisos para la notificacion si no los tengo ya
+                    ActivityCompat.requestPermissions(Activity_ajustes.this, new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 11);
+                    return;
+                }
+
+                NotificationManager notifManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                NotificationCompat.Builder elBuilder = new NotificationCompat.Builder(Activity_ajustes.this, "notifCanal");
+                // Creo el canal si la version de android lo requiere
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel elCanal = new NotificationChannel("notifCanal", "notifAjustes", NotificationManager.IMPORTANCE_DEFAULT);
+                    // Caracteristicas de la notificacion
+                    elBuilder.setSmallIcon(R.drawable.gold_trans)
+                            //.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.gold_trans))
+                            .setContentTitle("NUMERITOS")
+                            .setContentText(getString(R.string.ajustes_msg_notif))
+                            .setVibrate(new long[]{0, 1000, 500, 1000})
+                            .setAutoCancel(true);
+
+                    notifManager.createNotificationChannel(elCanal);
+                }
+
+                notifManager.notify(11, elBuilder.build());
+
                 Intent intent = new Intent();
                 setResult(3, intent);
                 finish();
